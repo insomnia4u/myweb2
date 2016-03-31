4658b625a6c956ebabacc24cd2f3564ff95c6806
insomnia4u <bcity@dongguk.edu>
2016-03-31 21:47:13 +0900

    1

--- src/sam/controller/SamController.java
+++ src/sam/controller/SamController.java
@@ -22,80 +22,80 @@ import java.util.*;
 
 public class SamController extends HttpServlet{
     
-   private Map commandMap;
+	private Map commandMap;
     
-   public SamController() {
-      commandMap=new HashMap<String,Object>();
-   }
-   @Override
-   public void init(ServletConfig config) throws ServletException {
-       String path=config.getServletContext().getRealPath("/WEB-INF/samCommand.properties");
-      // System.out.println("path="+path);
-       Properties prop=null;
-       try {
-         FileInputStream fis=
-               new FileInputStream(path);
-         prop=new Properties();
-         prop.load(fis);
-         fis.close();
-      }catch (FileNotFoundException e) {
-         e.printStackTrace();
-      }catch (IOException e) {
-         e.printStackTrace();
-      }
-       
-       Iterator keys=prop.keySet().iterator();
-       while(keys.hasNext()){
-          String key=(String)keys.next();
-          String classPath=prop.getProperty(key);
-          
-          try {
-            Class obj_class=Class.forName(classPath);
-            Object obj=obj_class.newInstance();
-            commandMap.put(key, obj);
-         } catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-         } catch (InstantiationException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-         } catch (IllegalAccessException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-         }
-       }
-   }
-   @Override
-   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public SamController() {
+		commandMap=new HashMap<String,Object>();
+	}
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+	    String path=config.getServletContext().getRealPath("/WEB-INF/samCommand.properties");
+	   // System.out.println("path="+path);
+	    Properties prop=null;
+	    try {
+			FileInputStream fis=
+					new FileInputStream(path);
+			prop=new Properties();
+			prop.load(fis);
+			fis.close();
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	    
+	    Iterator keys=prop.keySet().iterator();
+	    while(keys.hasNext()){
+	    	String key=(String)keys.next();
+	    	String classPath=prop.getProperty(key);
+	    	
+	    	try {
+				Class obj_class=Class.forName(classPath);
+				Object obj=obj_class.newInstance();
+				commandMap.put(key, obj);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         userProcess(req, resp);
-   }
-   @Override
-   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       userProcess(req, resp);
-   }
-   
-   protected void userProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      //1.http 요청받음(awt의 obj반환받음)
-      //String type=req.getParameter("type");
-      
-      String type=null;
-      String userUrl=req.getRequestURI();
-      if(userUrl.indexOf(req.getContextPath())==0){
-         type=userUrl.substring(req.getContextPath().length());
-      }
-      
-      //2.요구기능분석
-      CommandHandler command=
-            (CommandHandler)commandMap.get(type);
-      String goPage=null;
-      
-      
-      //4.결과저장
-      goPage=command.process(req, resp);
-      //5.이동
-      RequestDispatcher dis=
-            req.getRequestDispatcher(goPage);
-      dis.forward(req, resp);
-   }
-   
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	    userProcess(req, resp);
+	}
+	
+	protected void userProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//1.http ��û����(awt�� obj��ȯ����)
+		//String type=req.getParameter("type");
+		
+		String type=null;
+		String userUrl=req.getRequestURI();
+		if(userUrl.indexOf(req.getContextPath())==0){
+			type=userUrl.substring(req.getContextPath().length());
+		}
+		
+		//2.�䱸��ɺм�
+		CommandHandler command=
+				(CommandHandler)commandMap.get(type);
+		String goPage=null;
+		
+		
+		//4.�������
+		goPage=command.process(req, resp);
+		//5.�̵�
+		RequestDispatcher dis=
+				req.getRequestDispatcher(goPage);
+		dis.forward(req, resp);
+	}
+	
 }
