baa57ee26b8e07e8ec3f48d088cc897fc7613c41
insomnia4u <bcity@dongguk.edu>
2016-03-31 23:22:28 +0900

    11

---
+++ src/sam/emp/action/EmpAddAction2.java
@@ -0,0 +1,32 @@
+package sam.emp.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sam.command.CommandHandler;
+import sam.emp.model.EmpDAO;
+import sam.emp.model.EmpDTO;
+
+public class EmpAddAction2 implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name=req.getParameter("name");
+		String email=req.getParameter("email");
+		String dept=req.getParameter("dept");
+		
+		EmpDTO dto=new EmpDTO(0, name, email, dept);
+		EmpDAO dao=new EmpDAO();
+		int result=dao.empAdd(dto);
+		
+		String msg=result>0?"성공!":"실패!";
+		req.setAttribute("msg", msg);
+		
+			
+		return "/emp/empMsg.jsp";
+	}
+
+}
