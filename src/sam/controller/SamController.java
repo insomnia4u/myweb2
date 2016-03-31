670fa5a172c407522abddcc3bd04c27965ff0fd6
insomnia4u <bcity@dongguk.edu>
2016-03-31 19:05:52 +0900

    ddd

--- src/sam/controller/SamController.java
+++ src/sam/controller/SamController.java
@@ -12,90 +12,90 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import sam.command.CommandHandler;
-import sam.command.*;
+import sam.command.ContentAction;
+import sam.command.ListAction;
+
+import sam.command.WriteAction;
 import java.util.*;
 
 
+
 public class SamController extends HttpServlet{
-   
+    
    private Map commandMap;
-   
-   public SamController(){
+    
+   public SamController() {
       commandMap=new HashMap<String,Object>();
    }
-   
    @Override
    public void init(ServletConfig config) throws ServletException {
-      String path=config.getServletContext().getRealPath("/WEB-INF/samCommand.properties");
-      
-      //System.out.println("path="+path);
-      Properties prop=null;
-      try {
+       String path=config.getServletContext().getRealPath("/WEB-INF/samCommand.properties");
+      // System.out.println("path="+path);
+       Properties prop=null;
+       try {
          FileInputStream fis=
                new FileInputStream(path);
          prop=new Properties();
          prop.load(fis);
          fis.close();
-      } catch (FileNotFoundException e) {
+      }catch (FileNotFoundException e) {
          e.printStackTrace();
-      } catch (IOException e) {
+      }catch (IOException e) {
          e.printStackTrace();
       }
-      
-      Iterator keys=prop.keySet().iterator();
-      while(keys.hasNext()){
-         String key=(String)keys.next();
-         String classPath=prop.getProperty(key);
-         
-         try {
+       
+       Iterator keys=prop.keySet().iterator();
+       while(keys.hasNext()){
+          String key=(String)keys.next();
+          String classPath=prop.getProperty(key);
+          
+          try {
             Class obj_class=Class.forName(classPath);
             Object obj=obj_class.newInstance();
             commandMap.put(key, obj);
          } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
          } catch (InstantiationException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
          } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
          }
-      }
-      
+       }
    }
-   
    @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      userProcess(req, resp);
+        userProcess(req, resp);
    }
-   
    @Override
    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      userProcess(req, resp);
+       userProcess(req, resp);
    }
+   
    protected void userProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      //1.http 요청받음(awt의 object 반환받음)
+      //1.http 요청받음(awt의 obj반환받음)
       //String type=req.getParameter("type");
       
-	   String type=null;
-	   String userUrl=req.getRequestURI();
-      
-      if(userUrl.indexOf(req.getContextPath())==0)
-      {
-    	  type=userUrl.substring(req.getContextPath().length());
-    	  
+      String type=null;
+      String userUrl=req.getRequestURI();
+      if(userUrl.indexOf(req.getContextPath())==0){
+         type=userUrl.substring(req.getContextPath().length());
       }
       
       //2.요구기능분석
-      CommandHandler command=(CommandHandler)commandMap.get(type);
+      CommandHandler command=
+            (CommandHandler)commandMap.get(type);
       String goPage=null;
       
+      
+      //4.결과저장
       goPage=command.process(req, resp);
-      
       //5.이동
-      RequestDispatcher dis=req.getRequestDispatcher(goPage);
+      RequestDispatcher dis=
+            req.getRequestDispatcher(goPage);
       dis.forward(req, resp);
-      
-      
-   
-      
    }
+   
 }
