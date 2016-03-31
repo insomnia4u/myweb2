b7c8e3bb527e91df68de40d57b01573385aae008
insomnia4u <bcity@dongguk.edu>
2016-03-31 21:58:34 +0900

    set1

--- src/sam/emp/action/EmpAction.java
+++ src/sam/emp/action/EmpAction.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sun.org.apache.xalan.internal.xslt.Process;
+
 import sam.command.CommandHandler;
 
 public class EmpAction implements CommandHandler {
@@ -13,7 +15,7 @@ public class EmpAction implements CommandHandler {
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		return "/emp/emp.jsp";
+		return "emp/emp.jsp";
 	}
 
 }
